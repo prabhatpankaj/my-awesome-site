faa623998a5b8ed72dff0f867e9de9c182977540
prabhat pankaj <prabhatiitbhu@gmail.com>
2018-12-18 14:39:25 +0530

    Update entrypoint.sh

--- jenkins/entrypoint.sh
+++ jenkins/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 echo "starting dockerd..."
-sudo dockerd --host=unix:///var/run/docker.sock --host=$DOCKER_HOST --storage-driver=vfs &
+sudo dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --storage-driver=vfs &
 echo "starting jnlp slave..."
 exec java -jar /usr/share/jenkins/slave.jar \
-    -jnlpUrl $JENKINS_URL/computer/$JENKINS_SLAVE_NAME/slave-agent.jnlp \
-    -secret $JENKINS_SLAVE_SECRET
+	-jnlpUrl $JENKINS_URL/computer/$JENKINS_SLAVE_NAME/slave-agent.jnlp \
+	-secret $JENKINS_SLAVE_SECRET
